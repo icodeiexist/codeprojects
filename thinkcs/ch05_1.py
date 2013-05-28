667f4800fa41e7ffaa14877093c36662cd21cb8d
Rene Romero Benavides <rene.romero.b@gmail.com>
2013-05-28 10:16:29 -0500

    added ch05_1.py

---
+++ thinkcs/ch05_1.py
@@ -0,0 +1,43 @@
+def compare(a, b):
+    """
+      >>> compare(5, 4)
+      1
+      >>> compare(7, 7)
+      0
+      >>> compare(2, 3)
+      -1
+      >>> compare(42, 1)
+      1
+    """
+    return 1 if a > b else (-1 if a < b else 0)		
+    #  Your function body should begin here.
+
+def hypotenuse(a, b):
+    """
+      >>> hypotenuse(3, 4)
+      5.0
+      >>> hypotenuse(12, 5)
+      13.0
+      >>> hypotenuse(7, 24)
+      25.0
+      >>> hypotenuse(9, 12)
+      15.0
+    """
+    return (a**2+b**2)**0.5
+
+def slope(x1, y1, x2, y2):
+    """
+      >>> slope(5, 3, 4, 2)
+      1.0
+      >>> slope(1, 2, 3, 2)
+      0.0
+      >>> slope(1, 2, 3, 3)
+      0.5
+      >>> slope(2, 4, 1, 2)
+      2.0
+    """
+    return 0.0 if x1-x2 == 0 else abs(float(y1-y2)/float(x1-x2))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
