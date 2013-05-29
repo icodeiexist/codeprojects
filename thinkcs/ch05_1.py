55780e44d39ae2daee9624ef1d02a9534c90bffa
Rene Romero Benavides <rene.romero.b@gmail.com>
2013-05-29 09:17:19 -0500

    doctesting

--- thinkcs/ch05_1.py
+++ thinkcs/ch05_1.py
@@ -36,7 +36,23 @@ def slope(x1, y1, x2, y2):
       >>> slope(2, 4, 1, 2)
       2.0
     """
-    return 0.0 if x1-x2 == 0 else abs(float(y1-y2)/float(x1-x2))
+    if x1-x2 == 0:
+        return 0
+    else: 
+        s = float(y1-y2)/float(x1-x2)    
+        return 0.0 if s == -0 else s
+
+
+def intercept(x1, y1, x2, y2):
+    """
+      >>> intercept(1, 6, 3, 12)
+      3.0
+      >>> intercept(6, 1, 1, 6)
+      7.0
+      >>> intercept(4, 6, 12, 8)
+      5.0
+    """
+    return slope(x1,y1,x2,y2)*(-x1)+y1
 
 if __name__ == "__main__":
     import doctest
