fedef85376d0f9f19c90603ee60d22a847e26045
Rene Romero Benavides <rene.romero.b@gmail.com>
2013-05-29 23:37:00 -0500

    Discovered hyphen.Hyphenator - memo2.py

---
+++ mnemonic/mnemo2.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+"""
+mayor.py
+Codifica una palabra a su equivalente numérico siguiendo una adaptación al español del sistema mnemotécnico "mayor"
+http://en.wikipedia.org/wiki/Mnemonic_major_system
+Escrito por René Romero Benavides - @icodeiexist - GNU Public Licence
+
+Reglas fonéticas:
+1. t,d
+2. n,ñ
+3. m
+4. q,k,ca,co,cu,c[consonante]
+5. l,ll
+6. s,z,ce,ci
+7. j,f,ge,gi
+8. ch,ga,go,gu,sh
+9. v,p,b
+0. r
+
+Uso:
+python mayor.py word1 word2 wordN
+
+Ejemplos:
+python mayor.py "arrollo" "escarlatina" "canalla" "electricidad"
+05
+640512
+425
+5410611
+
+Code references:
+http://docs.python.org/2/howto/sorting.html
+http://docs.python.org/2/howto/regex.html
+http://docs.python.org/2/library/re.html#match-objects
+http://stackoverflow.com/questions/522563/accessing-the-index-in-python-for-loops
+http://www.tutorialspoint.com/python/string_replace.htm
+http://stackoverflow.com/questions/419163/what-does-if-name-main-do
+http://stackoverflow.com/questions/4574509/python-remove-duplicate-chars-using-regex
+"""
+import re,sys
+from hyphen import Hyphenator
+from hyphen.dictools import *
+
+for lang in ['es_MX', 'es_SP']:
+        if not is_installed(lang): install(lang)
+
+class major:
+    h_mx = hyphenator('es_MX')
+    codes = [('^r'),('^t','^d'),('^n','^ñ'),('^m',),('^c[^aeiouh]','^ca','^co','^cu','^k','^q'),('^l','^y'),('^s','^z','^ce','^ci'),('^j','^f','^ge','^gi'),('^ch','^ga','^go','^gu','^sh','^g[^aeiou]'),('^p','^v','^b')]
+   def encode(self,word):
+      matches = []
+      h_mx = hyphenator('es_MX')
+      for idx,code in enumerate(self.codes):
+         for pattern in code:
+            for syllable in h_mx.syllables(word)
+                itera = re.finditer(pattern,syllable)
+                if itera:
+                   for match in itera:
+                       matches.append((match.start(),idx))
+    matches_sorted = sorted(matches, key=lambda pos: pos[0])
+    num_string = ""
+    for start,idx in matches_sorted:
+          num_string += str(idx)
+    return num_string # ll and rr should account for l and r respectively
+
+if __name__ == "__main__":
+   m = major()
+   for word in sys.argv[1:]:
+      print m.encode(word)
